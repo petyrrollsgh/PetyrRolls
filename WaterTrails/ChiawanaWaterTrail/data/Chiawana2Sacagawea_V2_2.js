41def7fd85f7e7534a33f352a34e4450ef848ea6
Petyr <petyr_jk@charter.net>
2025-06-24 12:29:06 -0700

    Images added for Chiawana and Bend HMU.  New htlm doc added for Dent Wasteway with image stills from movie.  Still need to add Bourgans Island and give it all a check.

--- WaterTrails/ChiawanaWaterTrail/data/Chiawana2Sacagawea_V2_2.js
+++ WaterTrails/ChiawanaWaterTrail/data/Chiawana2Sacagawea_V2_2.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:530ded691dbfc8f590497ce774c1c3523465f4977d64c2ac0d50ccbaddbc82be
-size 3850
+oid sha256:58cea24383d0f34118d01e923a2c73d83aaa46bf19f9f80233fdcf1b4cd489f3
+size 3832
--- WaterTrails/ChiawanaWaterTrail/data/Selph2Chiawana_V2_3.js
+++ WaterTrails/ChiawanaWaterTrail/data/Selph2Chiawana_V2_3.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:000ab06dc563c79b5772cb5c9aa0e0d2a56437c284315c2cc4927e255355abb9
-size 3207
+oid sha256:9d6680b7b313b1a6cf9164f2c79eb1fb13dec38f64d22e85e1a402db4fcee5dd
+size 3164
