75b03c3a643a9855d8469290fd131383441d8ae1
Petyr <petyr_jk@charter.net>
2025-06-25 10:14:52 -0700

    Selph,Helms  and Dent HTMLdocs were separated and still photos added for Dent Wasteway.  Added marker, photos and HTMLdoc for Borgan's Island. All nav strips need consistent scroll process see selph for hints.  Photos in HTMLdocs are still distorted.

--- WaterTrails/ChiawanaWaterTrail/data/Chiawana2Sacagawea_V2_2.js
+++ WaterTrails/ChiawanaWaterTrail/data/Chiawana2Sacagawea_V2_2.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58cea24383d0f34118d01e923a2c73d83aaa46bf19f9f80233fdcf1b4cd489f3
-size 3832
+oid sha256:b5f1e474dbc0f7c11150a6de6abb712d61981dbe4d100232b68e75d042a3c95d
+size 4226
