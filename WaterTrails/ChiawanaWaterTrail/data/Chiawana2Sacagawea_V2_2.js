59399e676b11365e4f43935d4087993e3a22e059
alejandrocastaneda <alejandro.castaneda@wsu.edu>
2025-07-13 21:20:01 -0700

    Copy and push Pete's working and up-to-date files.

---
+++ WaterTrails/ChiawanaWaterTrail/data/Chiawana2Sacagawea_V2_2.js
@@ -0,0 +1,116 @@
+var json_Chiawana2Sacagawea_V2_2 = {
+	"type":"FeatureCollection",
+	"name":"Chiawana2Sacagawea_V2_2",
+	"crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},
+	"features":[
+		{"type":"Feature",
+		"properties":{
+			"id":"4",
+			"Name":"Sacagawea Beach",
+			"RiverMile":"326",
+			"Desc":"Swimming area with easy access to parking lot and Confluence Project Story Circles",
+			"PhotoPath":"images\\IMG_1240.jpeg",
+			"ExternLink":"https://parks.wa.gov/find-parks/state-parks/sacajawea-historical-state-park",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\SacagaweaPark.html"},
+		"geometry":{"type":"Point","coordinates":[-119.040130004505684,46.198538519974583]}},
+		
+		{"type":"Feature",
+		"properties":{
+			"id":"6",
+			"Name":"Sacagawea Boat Launch",
+			"RiverMile":"325",
+			"Desc":"Boat Launch can be weedy in late summer",
+			"PhotoPath":"images\\IMG_1263.jpeg",
+			"ExternLink":"https://parks.wa.gov/find-parks/state-parks/sacajawea-historical-state-park",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\SacagaweaPark.html"},
+		"geometry":{"type":"Point","coordinates":[-119.037331702279204,46.201497788238889]}},
+		
+		{"type":"Feature",
+		"properties":{
+			"id":"7",
+			"Name":"Schlagel Park",
+			"RiverMile":"328",
+			"Desc":"Boat Launch, Marina and all Facilities",
+			"PhotoPath":"images\\IMG_1211.jpeg",
+			"ExternLink":"https://www.pasco-wa.gov/Facilities/Facility/Details/29",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\SchlagelPark.html"},
+		"geometry":{"type":"Point","coordinates":[-119.091058214355215,46.21776612101845]}},
+		
+		{"type":"Feature",
+		"properties":{
+			"id":"8",
+			"Name":"Wade Park East",
+			"RiverMile":"330.5",
+			"Desc":"Nearby Parking but difficult water access",
+			"PhotoPath":"images\\IMG_1198.jpeg",
+			"ExternLink":"https://www.pasco-wa.gov/Facilities/Facility/Details/Wade-Park-37",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\WadePark.html"},
+		"geometry":{"type":"Point","coordinates":[-119.144084916427687,46.230339776536574]}},
+		
+		{"type":"Feature",
+		"properties":{
+			"id":"9",
+			"Name":"Wade Park West",
+			"RiverMile":"331.5",
+			"Desc":"Boat Ramp, Parking and Restrooms",
+			"PhotoPath":"images\\IMG_1195.jpeg",
+			"ExternLink":"https://www.pasco-wa.gov/Facilities/Facility/Details/Wade-Park-37",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\WadePark.html"},
+			"geometry":{"type":"Point","coordinates":[-119.162758038074273,46.232427806895771]}},
+
+		{"type":"Feature",
+		"properties":{
+			"id":"10",
+			"Name":"Chiawana Boat Ramp",
+			"RiverMile":" 333.5",
+			"Desc":"Boat Launch with Parking and Restrooms",
+			"PhotoPath":"images\\IMG_0921.jpeg",
+			"ExternLink":"https://www.nww.usace.army.mil/Locations/District-Locks-and-Dams/McNary-Lock-and-Dam/Chiawana-Park-Road-54-Park/",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\ChiawanaPark.html"},
+			"geometry":{"type":"Point","coordinates":[-119.205247244328376,46.243844385128661]}},
+			
+		{"type":"Feature",
+		"properties":{
+			"id":"2",
+			"Name":"Chiawana East",
+			"RiverMile":"334",
+			"Desc":"Chiawana East End Grassy Beach",
+			"PhotoPath":"images\\IMG_0920.jpeg",
+			"ExternLink":"https://www.pasco-wa.gov/Facilities/Facility/Details/Chiawana-Park-14",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\ChiawanaPark.html"},
+			"geometry":{"type":"Point","coordinates":[-119.207968766258944,46.244317013994532]}},
+			
+		{"type":"Feature",
+		"properties":{
+			"id":"3",
+			"Name":"Chiawana West",
+			"RiverMile":"335",
+			"Desc":"Dock and Rough Shoreline on West End of Chiawana Park.",
+			"PhotoPath":"images\\IMG_0919.jpeg",
+			"ExternLink":"https://www.pasco-wa.gov/Facilities/Facility/Details/Chiawana-Park-14",
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\ChiawanaPark.html"},
+		"geometry":{"type":"Point","coordinates":[-119.216810640893414,46.247723810389687]}},
+		
+		{"type":"Feature",
+		"properties":{
+			"id":"3",
+			"Name":"Borgan's Island",
+			"RiverMile":"331",
+			"Desc":"Wildlife Habitat - No public Acess",
+			"PhotoPath":"images\\IMG_1180.jpeg",
+			"ExternLink":null,
+			"SOSLink":null,
+			"PublicLink":"HTMLdocs\\BorgansIsland.html"},
+		"geometry":{"type":"Point","coordinates":[-119.19904771830232,46.23675711055839]}},
+		
+		
+	]
+}
